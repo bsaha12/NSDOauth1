3ebffdadbd3f21e2b7c6593c7fd18cbaf9b67e1c
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-01-26 12:06:08 +0530

    update

--- frontend/home.js
+++ frontend/home.js
@@ -1,8 +1,8 @@
 async function loginWithGitHub() {
   try {
-    const res = await fetch(
-      "https://github.com/login/oauth/authorize?client_id=9ded9dd55a8f77452b4b"
-    );
+    // const res = await fetch(
+      window.location.href = "https://github.com/login/oauth/authorize?client_id=9ded9dd55a8f77452b4b"
+    // );
     // window.location.href = res.url;
     console.log(res) ;
   } catch (error) {
